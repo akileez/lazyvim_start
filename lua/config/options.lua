79421765694457f3492551e2471e7f0dc4d39301
Keith Williams <keedax@gmail.com>
2024-08-24 02:36:45 -0400

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,31 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.mapleader = " "
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+
+vim.opt.showcmd = true
+vim.opt.expandtab = true
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = true
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = "cursor"
+-- Specifies formatting options. In this case, it adds the option to contiune comment lines started with `*` in a new line with the same indentation.
+vim.opt.formatoptions:append({ "r" })
